af6ba8e28cba36ea1eb49b982417b5343069b32d
a-moreira <arthrfrnco@gmail.com>
2020-07-11 23:37:12 -0300

    added support for config files

---
+++ tidy-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dotfiles=(".vimrc" ".gitconfig" ".fehbg" ".profile" ".bashrc")
+
+dir="${HOME}/Projects/dotfiles/tidy"
+
+for dotfile in "${dotfiles[@]}"; do
+    ln -s -f  "${dir}/${dotfile}" "$HOME/"
+done
+
+configs=("i3/config")
+
+for configuration in "${configs[@]}"; do
+    ln -s -f "${dir}/.config/{configuration}" "$HOME/.config/{configuration}"
+done
